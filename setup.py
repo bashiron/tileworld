c5503d062e4a1449e43978d7e8c2b2d1293c28cc
Bashiron <bioshiron@gmail.com>
2023-01-22 18:28:32 -0300

    example cli script

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ setup(
     name='tileworld',
     version='1.0.0',
     description='turn-based game',
-    py_modules=[],
     install_requires=[
         'zope.interface==5.5.2',
         'more-itertools==9.0.0'
-    ]
+    ],
+    entry_points='''
+        [console_scripts]
+        tileworld=cli:main
+    '''
+    # for some reason it let me specify the `cli` module without specifying the containing package (src)
 )
